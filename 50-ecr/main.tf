56e7710a9a344be128ed99c1d796f5b3e0c5c758
Javeed Mohammed <javeedmohd214@gmail.com>
2026-04-25 16:04:41 +0530

    k8

--- 50-ecr/main.tf
+++ 50-ecr/main.tf
@@ -1,68 +1,70 @@
-; # Catalogue ECR repository
-; resource "aws_ecr_repository" "catalogue" {
-;   name                 = "${var.project}/catalogue"
-;   image_tag_mutability = "MUTABLE"
-;   force_delete         = true
+# Catalogue ECR repository
+/*
+resource "aws_ecr_repository" "catalogue" {
+  name                 = "${var.project}/catalogue"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
-;   image_scanning_configuration {
-;     scan_on_push = false
-;   }
-; }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
 
-; # User ECR repository
-; resource "aws_ecr_repository" "user" {
-;   name                 = "${var.project}/user"
-;   image_tag_mutability = "MUTABLE"
-;   force_delete         = true
+# User ECR repository
+resource "aws_ecr_repository" "user" {
+  name                 = "${var.project}/user"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
-;   image_scanning_configuration {
-;     scan_on_push = false
-;   }
-; }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
 
-; # Cart ECR repository
-; resource "aws_ecr_repository" "cart" {
-;   name                 = "${var.project}/cart"
-;   image_tag_mutability = "MUTABLE"
-;   force_delete         = true
+# Cart ECR repository
+resource "aws_ecr_repository" "cart" {
+  name                 = "${var.project}/cart"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
-;   image_scanning_configuration {
-;     scan_on_push = false
-;   }
-; }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
 
-; # Shipping ECR repository
-; resource "aws_ecr_repository" "shipping" {
-;   name                 = "${var.project}/shipping"
-;   image_tag_mutability = "MUTABLE"
-;   force_delete         = true
+# Shipping ECR repository
+resource "aws_ecr_repository" "shipping" {
+  name                 = "${var.project}/shipping"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
-;   image_scanning_configuration {
-;     scan_on_push = false
-;   }
-; }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
 
-; # Payment ECR repository
-; resource "aws_ecr_repository" "payment" {
-;   name                 = "${var.project}/payment"
-;   image_tag_mutability = "MUTABLE"
-;   force_delete         = true
+# Payment ECR repository
+resource "aws_ecr_repository" "payment" {
+  name                 = "${var.project}/payment"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
-;   image_scanning_configuration {
-;     scan_on_push = false
-;   }
-; }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
 
-; # Frontend ECR repository
-; resource "aws_ecr_repository" "frontend" {
-;   name                 = "${var.project}/frontend"
-;   image_tag_mutability = "MUTABLE"
-;   force_delete         = true
+# Frontend ECR repository
+resource "aws_ecr_repository" "frontend" {
+  name                 = "${var.project}/frontend"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
-;   image_scanning_configuration {
-;     scan_on_push = false
-;   }
-; }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+*/
 
 # Backend and Frontend components Elastic Container Registry repositories
 resource "aws_ecr_repository" "repos" {
