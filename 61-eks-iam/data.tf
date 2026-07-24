dc366524da3dd45d718f0d7afc32b8db19e6fd85
Javeed Mohammed <javeedmohd214@gmail.com>
2026-07-24 18:28:15 +0530

    k8

---
+++ 61-eks-iam/data.tf
@@ -0,0 +1,11 @@
+# Fetch the EKS cluster to get the OIDC issuer URL
+data "aws_eks_cluster" "this" {
+  name = local.cluster_name
+}
+
+# Fetch the OIDC provider created by the EKS module
+data "aws_iam_openid_connect_provider" "this" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+data "aws_caller_identity" "current" {}
